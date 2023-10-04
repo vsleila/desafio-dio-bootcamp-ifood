8fa6a11968febb3aaaeb5f4614b3a2f2f332c5d9
Leila Vasconcelos <leila.vasconcelos.022@gmail.com>
2023-10-03 21:09:00 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-var teste1 = "biscoito";
-var teste2 = "bolacha"; 
+//Desafio do Felipao
 
-console.log(oi);
+    var nomeHeroi = "Nesuko Kamado";
+    var nivelHeroi;
+    var xp = 10000;
+
+        if (xp < 1.000){
+            nivelHeroi = "Ferro";
+        } else if ((xp >= 1001) && (xp <= 2000)){
+            nivelHeroi = "Bronze";
+        } else if ((xp >= 2001) && (xp <= 5000)){
+            nivelHeroi = "Prata";
+        } else if ((xp >= 6001) && (xp <= 7000)){
+            nivelHeroi = "Ouro";
+        } else if ((xp >= 7001) && (xp <= 8000)){
+            nivelHeroi = "Platina";
+        } else if ((xp >= 8001) && (xp <= 9000)){
+            nivelHeroi = "Ascendente";
+        } else if ((xp >= 9001) && (xp <= 10000)){
+            nivelHeroi = "Imortal";
+        } else if (xp >= 10001){
+            nivelHeroi = "Radiante";
+        }
+
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi);
